de8d11fe2bed618909d3fb75688434b8c8a08b05
nagesh-merva <officialnagesh.merva@gmail.com>
2024-01-16 21:43:11 +0530

    creating product.html

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-var navigatedPGlink = document.referrer;
+// var navigatedPGlink = document.referrer;
 
 window.addEventListener('load',function(){
   if(window.location.pathname ==='/product.html'){
-    var navigatedPGlink = document.referrer;
+    let navigatedPGlink = document.referrer;
     let link = document.getElementById("navigated-pg-link")
-    var fileName = navigatedPGlink.split('/').pop().replace('.html', '');
+    let fileName = navigatedPGlink.split('/').pop().replace('.html', '');
     link.href = navigatedPGlink
     link.textContent = fileName
+    let productImg = document.getElementById("product-img")
+    let atag =this.document.querySelector('a[href="./product.html"]')
+    let imgtag = atag.querySelector('img')
+    productImg.src = imgtag.src
   }
 })
 
