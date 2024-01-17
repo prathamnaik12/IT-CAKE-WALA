3d223b9f736ac6acaf32ae62f25a6e809b872b8f
nagesh-merva <officialnagesh.merva@gmail.com>
2024-01-17 23:18:07 +0530

    working on product.html

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 // var navigatedPGlink = document.referrer;
 
-window.addEventListener('load',function(){
-  if(window.location.pathname ==='/product.html'){
+document.addEventListener('DOMContentLoaded', function() {
+  if (window.location.pathname === '/product.html') {
     let navigatedPGlink = document.referrer;
-    let link = document.getElementById("navigated-pg-link")
+    let link = document.getElementById("navigated-pg-link");
     let fileName = navigatedPGlink.split('/').pop().replace('.html', '');
-    link.href = navigatedPGlink
-    link.textContent = fileName
-    let productImg = document.getElementById("product-img")
-    let atag =this.document.querySelector('a[href="./product.html"]')
-    let imgtag = atag.querySelector('img')
-    productImg.src = imgtag.src
+    link.href = navigatedPGlink;
+    link.textContent = fileName;
   }
-})
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  let productLinks = document.querySelectorAll('a[href="./product.html"]');
+  
+  productLinks.forEach(function(link) {
+    link.addEventListener('click', function() {
+      localStorage.setItem('clickedProductId', link.id);
+    });
+  });
+});
+
 
