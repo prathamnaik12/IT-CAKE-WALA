ae65e43388e64724d8d186f42c7ec4799fc6fd5b
nagesh-merva <officialnagesh.merva@gmail.com>
2024-01-19 23:13:09 +0530

    !

--- cart.js
+++ cart.js
@@ -2,7 +2,9 @@ let productname = document.getElementById("product-name")
 let productprice = document.getElementById("product-price")
 let productqty = document.getElementById("product-qty")
 
-document.addEventListener('DOMContentLoaded',function(){
+let cartbtn = document.getElementById("cart")
+
+cartbtn.addEventListener('onclick',function(){
     let addedProductName = localStorage.getItem('product_name')
     let addedProductprice = localStorage.getItem('product_price')
     let addedProductqty = localStorage.getItem('product_qty')
@@ -10,4 +12,5 @@ document.addEventListener('DOMContentLoaded',function(){
     productname.textContent = addedProductName
     productprice.textContent = addedProductprice
     productqty.setAttribute = addedProductqty
+    window.location.href= './cart.html'
 })
