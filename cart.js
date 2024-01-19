1478a137256a2e739f4ef0e06b8ab9a188a4a677
nagesh-merva <officialnagesh.merva@gmail.com>
2024-01-19 23:33:28 +0530

    !

--- cart.js
+++ cart.js
@@ -1,16 +1,17 @@
-let productname = document.getElementById("product-name")
-let productprice = document.getElementById("product-price")
-let productqty = document.getElementById("product-qty")
+let productname = document.getElementById("product-name");
+let productprice = document.getElementById("product-price");
+let productqty = document.getElementById("product-qty");
 
-let cartbtn = document.getElementById("cart")
+let cartbtn = document.getElementById("cart");
 
-cartbtn.addEventListener('click',function(){
-    let addedProductName = localStorage.getItem('product_name')
-    let addedProductprice = localStorage.getItem('product_price')
-    let addedProductqty = localStorage.getItem('product_qty')
+cartbtn.addEventListener('click', function () {
+    let addedProductName = localStorage.getItem('product_name');
+    let addedProductprice = localStorage.getItem('product_price');
+    // let addedProductqty = localStorage.getItem('product_qty');
 
-    productname.textContent = addedProductName
-    productprice.textContent = addedProductprice
-    productqty.setAttribute = addedProductqty
-    window.location.href= './cart.html'
-})
+    productname.textContent = addedProductName;
+    productprice.textContent = addedProductprice;
+    productqty.setAttribute('', addedProductqty); 
+
+    window.location.href = './cart.html';
+});
