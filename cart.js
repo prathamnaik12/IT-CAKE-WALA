04e4b0d47b156eb69dc73eec0491165edbd36dd9
nagesh-merva <officialnagesh.merva@gmail.com>
2024-01-21 17:10:30 +0530

    fixing issues in retrieving data in cart.html

--- cart.js
+++ cart.js
@@ -1,19 +1,23 @@
-let productname = document.getElementById("product-name");
-let productprice = document.getElementById("product-price");
-let productqty = document.getElementById("product-qty");
-let productImg = document.getElementById("product-img")
-let cartbtn = document.getElementById("cart");
 
-cartbtn.addEventListener('click', function () {
-    let addedProductName = localStorage.getItem('product_name');
-    let addedProductprice = localStorage.getItem('product_price');
-    let addedProductImg = localStorage.getItem('product_img')
-    console.log(addedProductName)
-    console.log(addedProductprice)
-    console.log(addedProductImg)
-    productname.textContent = addedProductName;
-    productprice.textContent += addedProductprice;
-    productImg.src = addedProductImg
+document.addEventListener('DOMContentLoaded', function () {
+    let productname = document.getElementById("product-name")
+    let productprice = document.getElementById("product-price")
+    let productImg = document.getElementById("product-img")
+    let cartbtn = document.getElementById("cart")
 
-    window.location.href = './cart.html';
-});
+    cartbtn.addEventListener('click', function () {
+        let addedProductName = localStorage.getItem('product_name')
+        let addedProductprice = localStorage.getItem('product_price')
+        let addedProductImg = localStorage.getItem('product_img')
+
+        console.log(addedProductName)
+        console.log(addedProductprice)
+        console.log(addedProductImg)
+
+        productname.textContent = addedProductName
+        productprice.textContent = addedProductprice
+        productImg.src = addedProductImg
+
+        window.location.href = './cart.html'
+    })
+})
